37bb95963fe8a41f13c5799216ba25d0fd8f1f0d
eweracs <sebastian.carewe@gmail.com>
2025-02-26 23:51:03 +0100

    Proper straight/curve handling, faster calculations, fix shear method

--- Italify.glyphsFilter/Contents/Resources/plugin.py
+++ Italify.glyphsFilter/Contents/Resources/plugin.py
@@ -72,48 +72,111 @@ class Italify(FilterWithDialog):
 			add_extremes = bool(customParameters.get("extremes", False))
 
 		rotation_angle = angle * ratio
-		slant_angle = angle * (1 - ratio)
+		shear_angle = angle * (1 - ratio)
 		bounds = layer.bounds
-		center = NSMakePoint(NSMidX(bounds), NSMidY(bounds))
+		centre = NSMakePoint(NSMidX(bounds), NSMidY(bounds))
 
 		for path in layer.paths:
-			self.process_path(path, center, angle, rotation_angle, slant_angle, distinguish_straight_and_curved)
+			self.process_path(path, centre, angle, distinguish_straight_and_curved, rotation_angle, shear_angle)
 
 		for anchor in layer.anchors:
-			self.slant_node(center, angle, anchor)
+			anchor.position = self.shear_point(centre, angle, anchor.position)
 
 		if add_extremes:
 			layer.addExtremePoints()
 
-	@objc.python_method
-	def process_path(self, path, center, angle, rotation_angle, slant_angle, distinguish_straight_and_curved):
-		node_count = len(path.nodes)
-		for i in range(node_count):
-			node = path.nodes[i]
-			prev_node = path.nodes[(i - 1) % node_count]
-			next_node = path.nodes[(i + 1) % node_count]
-
-			if distinguish_straight_and_curved:
-				if node.type != "offcurve":
-					if prev_node.type != "offcurve" and next_node.type != "offcurve":
-						# Straight segment
-						self.process_straight_segment(path, i, center, angle)
-					else:
-						# Curved segment
-						self.process_curved_segment(node, prev_node, next_node, center, rotation_angle, slant_angle)
-			else:
-				self.rotate_node(center, rotation_angle, node)
-				self.slant_node(center, slant_angle, node)
+		layer.roundCoordinates()
 
 	@objc.python_method
-	def process_straight_segment(self, path, index, center, angle):
-		node = path.nodes[index]
-		next_node = path.nodes[(index + 1) % len(path.nodes)]
+	def process_path(self, path, centre, angle, distinguish_straight_and_curved, rotation_angle, shear_angle):
+		if distinguish_straight_and_curved:
+			self.smart_italify(path, centre, angle, rotation_angle, shear_angle)
+			
+		else:
+			self.simple_italify(path, centre, rotation_angle, shear_angle)
+				
+	@objc.python_method
+	def smart_italify(self, path, centre, angle, rotation_angle, shear_angle):
+		# Get path segments
+		transformed_segments = []
+		path_segments = self.split_path_into_segments(path)
+		for segment in path_segments:
+			proxy_segment = [node.position for node in segment]
+			# process each segment
+			# process straight segment
+			if len(segment) == 2:
+				transformed_segment = self.process_straight_segment(proxy_segment, centre, angle)
+				transformed_segments.append(transformed_segment)
 
-		proxy_segment = [GSNode(node.position), GSNode(next_node.position)]
+			# process curved segment
+			elif len(segment) == 4:
+				transformed_segment = self.rotate_and_shear(proxy_segment, centre, rotation_angle, shear_angle)
+				transformed_segments.append(transformed_segment)
 
-		dx = next_node.x - node.x
-		dy = next_node.y - node.y
+		proxy_layer = GSLayer()
+		path_list = []
+		for transformed_segment in transformed_segments:
+			if transformed_segment[0] == transformed_segment[1]:
+				continue
+			segment_path = self.make_path_from_segment(transformed_segment)
+			proxy_layer.paths.append(segment_path)
+			path_list.append(segment_path)
+
+		for index in range(len(proxy_layer.paths) - 1):
+			proxy_layer.connectPathsWithNode_andNode_extendPath_(
+				list(proxy_layer.paths[0].nodes)[-1],
+				proxy_layer.paths[1].nodes[0],
+				True
+			)
+		proxy_layer.connectPathsWithNode_andNode_extendPath_(
+			list((list(proxy_layer.paths)[-1]).nodes)[-1],
+			list(proxy_layer.paths[0].nodes)[0],
+			True
+		)
+
+		for shape in proxy_layer.shapes:
+			path.parent.background.shapes.append(shape)
+
+		# Add transformed segments to the background
+		# self.visualise_segments(transformed_segments, path.parent)
+
+
+	@objc.python_method
+	def simple_italify(self, path, centre, rotation_angle, shear_angle):
+		for node in path.nodes:
+			node.position = self.rotate_point(centre, rotation_angle, node.position)
+			node.position = self.shear_point(centre, shear_angle, node.position)
+
+	@objc.python_method
+	def make_path_from_segment(self, segment):
+		new_path = GSPath()
+		for index, point in enumerate(segment):
+			node = GSNode(point)
+			if len(segment) == 2:
+				node.type = LINE
+			if len(segment) == 4:
+				if index == 0:
+					node.type = LINE
+				if index == 1 or index == 2:
+					node.type = OFFCURVE
+				if index == 3:
+					node.type = CURVE
+			new_path.addNode_(node)
+		return new_path
+
+	@objc.python_method
+	def visualise_segments(self, segments, layer):
+		for segment in segments:
+			new_path = self.make_path_from_segment(segment)
+			layer.background.paths.append(new_path)
+
+	@objc.python_method
+	def process_straight_segment(self, segment, centre, angle):
+		point_1 = segment[0]
+		point_2 = segment[1]
+
+		dx = point_2.x - point_1.x
+		dy = point_2.y - point_1.y
 
 		# Calculate the segment angle
 		segment_angle = atan2(dy, dx)
@@ -121,59 +184,62 @@ class Italify(FilterWithDialog):
 		# Convert to degrees and normalize to 0-180 range
 		angle_deg = (segment_angle * 180 / pi) % 180
 
-		normalised_angle = abs(angle_deg % 180)
-		if normalised_angle > 90:
-			normalised_angle = 180 - normalised_angle
-
 		# Calculate shear and rotation factors
 		# 0 degrees (horizontal) -> full shear, no rotation
 		# 90 degrees (vertical) -> full rotation, no shear
-		shear_factor = cos(radians(normalised_angle))
-		rotation_factor = sin(radians(normalised_angle))
+		shear_factor = cos(radians(angle_deg))
+		rotation_factor = sin(radians(angle_deg))
 
 		# Apply transformations
 		shear_angle = angle * shear_factor
 		rotation_angle = angle * rotation_factor
 
-		print("Segment angle:", angle_deg, "Shear angle:", shear_angle, "Rotation angle:", rotation_angle)
+		transformed_segment = self.rotate_and_shear(segment, centre, rotation_angle, shear_angle)
 
-		# Apply rotation first
-		self.rotate_node(center, rotation_angle, node)
-		self.rotate_node(center, rotation_angle, next_node)
-
-		# Then apply shear
-		self.slant_node(center, shear_angle, node)
-		self.slant_node(center, shear_angle, next_node)
+		return transformed_segment
 
 	@objc.python_method
-	def process_curved_segment(self, node, prev_node, next_node, center, rotation_angle, slant_angle):
-		self.rotate_node(center, rotation_angle, node)
-		self.slant_node(center, slant_angle, node)
+	def rotate_and_shear(self, segment, centre, rotation_angle, shear_angle):
+		transformed_segment = []
+		for point in segment:
+			rotated_point = self.rotate_point(centre, rotation_angle, point)
+			sheared_point = self.shear_point(centre, shear_angle, rotated_point)
+			transformed_segment.append(sheared_point)
+		return transformed_segment
 
-		if prev_node.type == "offcurve":
-			self.rotate_node(center, rotation_angle, prev_node)
-			self.slant_node(center, slant_angle, prev_node)
+	@objc.python_method
+	def split_path_into_segments(self, path):
+		segments = []
+		for node in path.nodes:
+			if node.type != OFFCURVE:
+				if node.nextNode.type != OFFCURVE:
+					# if node.position == node.nextNode.position:
+					# 	continue
+					segment = [node, node.nextNode]
+					segments.append(segment)
+				if node.nextNode.type == OFFCURVE:
+					segment = [node, node.nextNode, node.nextNode.nextNode, node.nextNode.nextNode.nextNode]
+					segments.append(segment)
+		return segments
 
-		if next_node.type == "offcurve":
-			self.rotate_node(center, rotation_angle, next_node)
-			self.slant_node(center, slant_angle, next_node)
-
-	objc.python_method
-	def slant_node(self, center, angle, node):
+	@objc.python_method
+	def shear_point(self, centre, angle, point):
 		transform = NSAffineTransform.new()
-		transform.translateXBy_yBy_(center.x, center.y)
-		slant = tan(angle * pi / 180.0)
-		transform.shearXBy_(slant)
-		transform.translateXBy_yBy_(-center.x, -center.y)
-		node.position = transform.transformPoint_(node.position)
+		shear = tan(radians(angle))
+		transform.shearXBy_atCenter_(shear, centre.y)
+		transformed_point = transform.transformPoint_(point)
+
+		return transformed_point
 
 	@objc.python_method
-	def rotate_node(self, center, angle, node):
+	def rotate_point(self, centre, angle, point):
 		rotate = NSAffineTransform.new()
-		rotate.translateXBy_yBy_(center.x, center.y)
+		rotate.translateXBy_yBy_(centre.x, centre.y)
 		rotate.rotateByDegrees_(-angle)
-		rotate.translateXBy_yBy_(-center.x, -center.y)
-		node.position = rotate.transformPoint_(node.position)
+		rotate.translateXBy_yBy_(-centre.x, -centre.y)
+		transformed_point = rotate.transformPoint_(point)
+
+		return transformed_point
 
 	@objc.python_method
 	def customParameterString(self):
